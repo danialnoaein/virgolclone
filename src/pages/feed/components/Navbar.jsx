0d9ba954d61e67f6b4cd824cc7d8b017f8d70e6b
Danial <sdnoaein@gmail.com>
2022-09-16 13:13:43 +0430

    feat: add navbar

---
+++ src/pages/feed/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Styles from "../Styles.module.css";
+
+const Navbar = () => {
+    return (
+        <div className={`${Styles.navbar}`}>
+            <ul>
+                <li>
+                    <a href="#">جدیدا</a>
+                </li>
+                <li>
+                    <a href="#">جدیدا</a>
+                </li>
+                <li>
+                    <a href="#">جدیدا</a>
+                </li>
+                <li>
+                    <a href="#">جدیدا</a>
+                </li>
+                <li>
+                    <a href="#">جدیدا</a>
+                </li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Navbar;
